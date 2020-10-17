e72550d4aa33f66ee36909c731c2cd81fa775ca3
Gh-Adam <ghada.adam.2011@gmail.com>
2020-10-17 03:52:32 +0200

    Use React Icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FaAngellist } from 'react-icons/fa';
+import { FiAlertOctagon } from 'react-icons/fi' 
+import { IconContext } from 'react-icons/lib';
+import { BsFillCollectionFill } from 'react-icons/bs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IconContext.Provider value={{color: 'blue', size: '6rem'}} >
+        <FaAngellist color="red" size="10rem" />
+        <BsFillCollectionFill />
+        <FiAlertOctagon />
+      </IconContext.Provider>
     </div>
   );
 }
